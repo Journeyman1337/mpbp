1bc42cce8054050a63229c73b6249dff6e56d4fc
Daniel Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2022-08-02 15:52:42 -0400

    remove indenting of hash directives from lint
    this looks ugly as crap, get rid of it!

--- include/mpbp/pack_rect.hpp
+++ include/mpbp/pack_rect.hpp
@@ -19,35 +19,31 @@
 */
 
 #ifndef MPBP_PACK_RECT_HPP
-#  define MPBP_PACK_RECT_HPP
+#define MPBP_PACK_RECT_HPP
 
-#  include <compare>
-#  include <cstddef>
 #include <cmath>
+#include <compare>
+#include <cstddef>
 
 namespace mpbp
 {
-    struct pack_rect
+  struct pack_rect
+  {
+    std::size_t x = 0;
+    std::size_t y = 0;
+    std::size_t z = 0;
+    std::size_t width = 0;
+    std::size_t height = 0;
+
+    constexpr pack_rect() noexcept = default;
+
+    constexpr pack_rect(const std::size_t x, const std::size_t y, const std::size_t z,
+                        const std::size_t width, const std::size_t height) noexcept
+        : x(x), y(y), z(z), width(width), height(height)
     {
-      std::size_t x = 0;
-      std::size_t y = 0;
-      std::size_t z = 0;
-      std::size_t width = 0;
-      std::size_t height = 0;
-
-      constexpr pack_rect() noexcept = default;
-
-      constexpr pack_rect(const std::size_t x, const std::size_t y, const std::size_t z,
-                          const std::size_t width, const std::size_t height) noexcept
-          : x(x), y(y), z(z), width(width), height(height)
-      {
-      }
-
-    constexpr std::size_t
-    MaxDimension() const noexcept
-    {
-      return std::max(width, height);
     }
+
+    constexpr std::size_t MaxDimension() const noexcept { return std::max(width, height); }
 
     constexpr std::size_t FarX() const noexcept { return this->x + this->width; }
 
@@ -63,6 +59,6 @@ namespace mpbp
       return (this->width >= other.width) && (this->height >= other.height);
     }
   };
-}
+}  // namespace mpbp
 
 #endif
