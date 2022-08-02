89041d5b42ed509853883808883bb0792a0bf9b8
Daniel Valcour <60055347+Journeyman-dev@users.noreply.github.com>
2022-08-02 18:31:00 -0400

    impl the example
    pack some rects and print the result to the terminal

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -17,3 +17,48 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 */
+
+#include <mpbp/mpbp.hpp>
+#include <iostream>
+
+int main()
+{
+    std::vector<mpbp::input_rect> input_rects = 
+    {
+        mpbp::input_rect(0, 1, 1),
+        mpbp::input_rect(1, 2, 2),
+        mpbp::input_rect(2, 4, 4),
+        mpbp::input_rect(3, 5, 7),
+        mpbp::input_rect(4, 9, 10),
+        mpbp::input_rect(5, 1, 1)
+    };
+
+    mpbp::Packer packer;
+    packer.SetSize(32, 16);
+    packer.Pack(input_rects);
+
+    for (int y = 0; y < 16; y ++)
+    {
+        for (int x = 0; x < 32; x++)
+        {
+            bool rect_found = false;
+            for (const auto& rect : input_rects)
+            {
+                if (rect.x <= x && rect.y <= y && rect.far_x() >= x && rect.far_y() >= y)
+                {
+                    std::cout << rect.identifier;
+                    rect_found = true;
+                    break;
+                }
+            }
+            if (!rect_found)
+            {
+                std::cout << ".";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+
+    return 0;
+}
